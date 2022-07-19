1083cb34f819502d71c44f8e3cdeb33414699427
Aakash Verma <mecskyverse@gmail.com>
2022-07-19 18:50:30 +0530

    Initial Hardhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-solhint");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
