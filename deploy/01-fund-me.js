bec712c3bc1425c6aae5fe8c872c70856f2b61fd
Aakash <vermaaakash1000@gmail.com>
2022-07-21 18:19:49 +0530

    deployement function modified

--- deploy/01-fund-me.js
+++ deploy/01-fund-me.js
@@ -1,5 +1,11 @@
-function deployfunc() {
-  console.log("hi");
-}
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
 
-module.exports.default = deployfunc;
+  const fundMe = await deployments("FundME", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+};
