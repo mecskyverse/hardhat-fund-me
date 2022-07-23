7152029e1cfb083f38366b3cc8fd6f7a68f99c39
Aakash <vermaaakash1000@gmail.com>
2022-07-23 15:55:02 +0530

    Imported helper-hardhat for eth ud price feed address

--- deploy/01-fund-me.js
+++ deploy/01-fund-me.js
@@ -1,7 +1,14 @@
+const { networkconfig } = require("../helper-hardhat-config");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
+
+  //if chainId is X use address Y
+  //if chainId is Z use address L
+
+  const ethUsdPriceFeedAddress = networkconfig[chainId]["ethUsdPriceFeed"];
 
   const fundMe = await deployments("FundME", {
     from: deployer,
