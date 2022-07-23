6d383dad0b6e30be9c23a5be36794cda63a054d0
Aakash <vermaaakash1000@gmail.com>
2022-07-23 15:56:31 +0530

    solidity compiler 0,6.6 added

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,9 @@ require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.8",
+  //solidity: "0.8.8",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
   defaultNetwork: "hardhat",
 };
