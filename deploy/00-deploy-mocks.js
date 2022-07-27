e523ff2282d71cfeca507d1c8632620607665818
Aakash <vermaaakash1000@gmail.com>
2022-07-27 18:55:35 +0530

    Mock network specifications added

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,7 +1,28 @@
 // const { network } = require("hardhat");
 
+const { network } = require("hardhat");
+const {
+  developmentChains,
+  Decimals,
+  InitialAnswer,
+} = require("../helper-hardhat-config");
+
 module.exports = async ({ getNamedAccounts, deployements }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-  const chainId = network.config.chainId;
+  // const chainId = network.config.chainId;
+
+  if (developmentChains.includes(network.name)) {
+    log("Local Network detecetd Deploying mocks...");
+    await deploy("MockV3Aggregator", {
+      contract: "MockV3Aggregator",
+      from: deployer,
+      log: true,
+      args: [Decimals, InitialAnswer],
+    });
+    log("Mocks Deployed!");
+    log("----------------------------------------------");
+  }
 };
+
+module.exports.tags = ["all", "mocks"];
