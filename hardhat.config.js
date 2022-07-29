743118977d12bdb59ed9ccbfac2cf1c4c297b464
Aakash <vermaaakash1000@gmail.com>
2022-07-29 22:45:58 +0530

    rinkeby network added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,43 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-solhint");
+// require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+require("solidity-coverage");
 require("hardhat-deploy");
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const COINMARKETCAP_API_KEY = process.env.coinmarketapi;
+
+const RINKEBY_RPC_URL = "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY =
+  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 module.exports = {
-  //solidity: "0.8.8",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    // kovan: {
+    //   url: KOVAN_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 42,
+    //   blockConfirmations: 6,
+    //   gas: 6000000,
+    // },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+    },
+  },
   solidity: {
     compilers: [
       {
@@ -14,15 +48,16 @@ module.exports = {
       },
     ],
   },
-
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      // gasPrice: 130000000000,
-    },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
-
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
